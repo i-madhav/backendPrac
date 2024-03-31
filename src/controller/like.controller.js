a12939849f6eafa01b54cf37125f3862e433a53a
Madhav <madhav.shar06ma@gmail.com>
2024-03-31 21:54:49 +0530

    backend

--- src/controller/like.controller.js
+++ src/controller/like.controller.js
@@ -1,28 +1,109 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { Like } from "../models/like.modal.js"
+import { Video } from "../models/video.modal.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { registerUser } from "./user.controller.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
+    const { videoId } = req.params
     //TODO: toggle like on video
+    const user = await Like.findOne({
+        video: videoId,
+        likedBy: req.user?._id
+    })
+
+    if (user) {
+        await Like.deleteOne({
+            video: videoId,
+            likedBy: req.user?._id
+        })
+    }
+
+    const likedNow = await Like.create({
+        video: videoId,
+        likedBy: req.user?._id
+    })
+
+    if (!likedNow.likedBy) throw new ApiError(401, "failed to like video");
+
+    return res
+        .status(200)
+        .json(200, likedNow, "video liked successfully")
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
+    const { commentId } = req.params
     //TODO: toggle like on comment
 
+    const existedUser = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user?._id
+    })
+
+    if (existedUser) {
+        await Like.deleteOne({
+            comment: commentId,
+            likedBy: req.user?._id
+        })
+    }
+
+    const likedNow = await Like.create({
+        comment: commentId,
+        likedBy: req.user?._id
+    })
+
+    if (!likedNow) throw new ApiError(401, "failed to like video");
+
+    return res
+        .status(200)
+        .json(200, likedNow, "video liked successfully")
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
+    const { tweetId } = req.params
     //TODO: toggle like on tweet
+
+    const existedUser = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user?._id
+    })
+
+    if (existedUser) {
+        await Like.deleteOne({
+            tweet: tweetId,
+            likedBy: req.user?._id
+        })
+    }
+
+    const likedNow = await Like.create({
+        tweet: tweetId,
+        likedBy: req.user?._id
+    })
+
+    if (!likedNow) throw new ApiError(401, "failed to like video");
+
+    return res
+        .status(200)
+        .json(200, likedNow, "video liked successfully")
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const userLikes = await Like.find({
+        likedBy:req.user?._id
+    })
+   const videoId =  userLikes.map(likeObject => likeObject.video)
+
+   const video = Video.find({
+    owner:videoId
+   })
+   
+    return res.
+    status(200)
+    .json(200 , video , "liked vidoe fetched successfully")
 })
 
 export {
--- src/controller/tweet.controller.js
+++ src/controller/tweet.controller.js
@@ -1,24 +1,74 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweetSchema.modal.js"
+import { User } from "../models/user.modal.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { text } from "express"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    // body
+    // content mai daal duga
+    // aggregation pipeline kei through
+    // verifyjwt ka use karke user lauga
+
+    const { tweetContent } = req.body;
+    if (!tweetContent) throw new ApiError(400, "Enter content in order to tweet")
+
+    const user = await User.findById(req.user?._id);
+
+    if(!user) throw new ApiError(404 , "user not found")
+
+    const tweet = await Tweet.create({
+        content:tweetContent,
+        owner:user._id
+    })
+
+    if(!tweet) throw new ApiError(500 , "Can not create tweet");
+
+    return res
+    .status(200)
+    .json(200 , tweet , "tweet generated successfully");
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+
+    const tweetUser = await Tweet.find({owner:req.user?._id});
+
+    if(!tweetUser) throw new ApiError(400 , "user not found")
+    return res
+    .status(200)
+    .json(200 , tweetUser , "userTweetFetched successfully")
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const{newContent} = req.body;
+    const tweetUser = await Tweet.find({owner:req.user?._id});
+
+    tweetUser.content = newContent;
+    await tweetUser.save({ ValidateBeforeSave: false });
+
+    return res
+    .status(200)
+    .json(200 , tweetUser , "tweet updated sucessfully")
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const {tweetId} = req.param;
+    const tweet = await Tweet.findOneAndDelete({
+        _id:tweetId,
+        owner:req.user?._id
+    })
+
+    if(!tweet) throw new ApiError("Tweet can't be deleted")
+    
+    return res
+    .status(200)
+    .json(200 , null , "tweet deleted successfully")
 })
 
 export {
